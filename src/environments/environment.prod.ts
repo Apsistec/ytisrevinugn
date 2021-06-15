ee5cbf4f30b1752c0644d2b2caa93aa643c0efc1
Doug White <doug@apsistec.app>
2021-06-14 19:11:12 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig = {
+    apiKey: 'AIzaSyDv5e5I1FxWMJESwAvJHQ7MzC455tjnQXU',
+    authDomain: 'ng-university-1afd6.firebaseapp.com',
+    projectId: 'ng-university-1afd6',
+    storageBucket: 'ng-university-1afd6.appspot.com',
+    messagingSenderId: '721767185542',
+    appId: '1:721767185542:web:45357fb0c3bbbf000c2507'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDv5e5I1FxWMJESwAvJHQ7MzC455tjnQXU',
+    authDomain: 'ng-university-1afd6.firebaseapp.com',
+    projectId: 'ng-university-1afd6',
+    storageBucket: 'ng-university-1afd6.appspot.com',
+    messagingSenderId: '721767185542',
+    appId: '1:721767185542:web:45357fb0c3bbbf000c2507'
+  }
+};
